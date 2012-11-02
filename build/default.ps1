600bbd07d007331ca7b064069033148294bb7620
sjwood <octocat@nym.hush.com>
2012-11-02 13:42:08 +0000

    Added property validation to psake wrapper

--- build/default.ps1
+++ build/default.ps1
@@ -22,6 +22,17 @@ Properties {
 }
 
 
+$AllowedConfigurations = @("Debug", "Release")
+# Note 1:
+# There is an inconsistency between Solution and Project Platform values, so use Project definition for now
+# see http://connect.microsoft.com/VisualStudio/feedback/details/503935/msbuild-inconsistent-platform-for-any-cpu-between-solution-and-project
+#
+# Note 2:
+# platform specific builds (i.e. "x86", "x64", "Itanium") are not supported at present
+#
+$AllowedPlatforms = @("AnyCPU")
+
+
 $Framework = '4.0'
 $RootDirectory = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList ".."
 $ObjectDirectory = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList "$RootDirectory\obj"
@@ -46,7 +57,36 @@ Task Clean -Description "Cleans all build products" {
     }
 }
 
-Task Build -Depends Clean -Description "Builds all code" {
+
+Task ValidateProperties -Description "Validates script properties" {
+    $ConfigurationIsValid = $False
+    :AllowedConfigurationLoop foreach ($AllowedConfiguration in $AllowedConfigurations) {
+        if ($Configuration -eq $AllowedConfiguration)
+        {
+            $ConfigurationIsValid = $True
+            break AllowedConfigurationLoop
+        }
+    }
+    if ($ConfigurationIsValid -eq $False)
+    {
+        Throw "'$Configuration' is not a valid configuration. Valid configurations are $AllowedConfigurations."
+    }
+    $PlatformIsValid = $False
+    :AllowedPlatformLoop foreach ($AllowedPlatform in $AllowedPlatforms) {
+        if ($Platform -eq $AllowedPlatform)
+        {
+            $PlatformIsValid = $True
+            break AllowedPlatformLoop
+        }
+    }
+    if ($PlatformIsValid -eq $False)
+    {
+        Throw "'$Platform' is not a valid platform. Valid platforms are $AllowedPlatforms."
+    }
+}
+
+
+Task Build -Depends ValidateProperties, Clean -Description "Builds all code" {
     :SolutionLoop foreach ($Solution in $Solutions) {
         Write-Output ("  Building solution '" + $Solution.FullName + "'")
         Exec { MsBuild $Solution.FullName /nologo /verbosity:minimal /maxcpucount /p:Configuration=$Configuration /p:Platform=$Platform }
