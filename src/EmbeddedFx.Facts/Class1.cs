795828c4868733bc0c0353bbc9b6c034551f607a
sjwood <octocat@nym.hush.com>
2012-11-20 13:21:14 +0000

    Removed in-source suppressions and switched to StyleCop settings file

--- src/EmbeddedFx.Facts/Class1.cs
+++ src/EmbeddedFx.Facts/Class1.cs
@@ -1,8 +1,5 @@
-﻿[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:FileMustHaveHeader", Justification = "Reviewed. Suppression is OK here.")]
-
-namespace EmbeddedFx.Facts
+﻿namespace EmbeddedFx.Facts
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
     public class Class1
     {
     }
--- src/EmbeddedFx/EmbeddedAssemblyLoader.cs
+++ src/EmbeddedFx/EmbeddedAssemblyLoader.cs
@@ -1,10 +1,7 @@
-﻿[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:FileMustHaveHeader", Justification = "Reviewed. Suppression is OK here.")]
-
-namespace EmbeddedFx
+﻿namespace EmbeddedFx
 {
     using System;
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
     public sealed class EmbeddedAssemblyLoader
     {
         public EmbeddedAssemblyLoader()
