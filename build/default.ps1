415df197cdd1aba3f968c7e13a735450d550b02a
sjwood <octocat@nym.hush.com>
2013-07-15 15:55:18 +0100

    Created BuildHelper object. Tidied up PoSh build script.

--- build/default.ps1
+++ build/default.ps1
@@ -19,11 +19,10 @@ Set-StrictMode -Version Latest
 Properties {
     $Configuration = "Debug"
     $Platform = "AnyCPU"
-    $XunitVersion = "1.9.1"
 }
 
 
-$AllowedConfigurations = @("Debug", "Release")
+$Global:AllowedConfigurations = "Debug", "Release"
 # Note 1:
 # There is an inconsistency between Solution and Project Platform values, so use Project definition for now
 # see http://connect.microsoft.com/VisualStudio/feedback/details/503935/msbuild-inconsistent-platform-for-any-cpu-between-solution-and-project
@@ -31,76 +30,175 @@ $AllowedConfigurations = @("Debug", "Release")
 # Note 2:
 # platform specific builds (i.e. "x86", "x64", "Itanium") are not supported at present
 #
-$AllowedPlatforms = @("AnyCPU")
+$Global:AllowedPlatforms = "AnyCPU"
 
 
 Framework "4.0"
 
 
-$RootDirectory = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList ".."
-$ObjectDirectory = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList "$RootDirectory\obj"
-$BinariesDirectory = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList "$RootDirectory\bin"
-
-
-$Solutions = Get-ChildItem $RootDirectory.FullName -Include *.sln -Recurse
-
-
 Task default -Depends Test
 
 
-Task Clean -Description "Cleans all build products" {
-    $Directories = @($ObjectDirectory, $BinariesDirectory)
-    :DirectoryLoop foreach ($Directory in $Directories) {
-        if ($Directory.Exists)
-        {
-            Write-Output ("  Removing directory '" + $Directory.FullName + "'")
-            Remove-Item $Directory.FullName -Force -Recurse
-        }
-    }
+Task Clean -Depends InstantiateBuildHelper -Description "Cleans all build products" {
+    $Global:BuildHelper.Clean()
 }
 
 
-Task ValidateProperties -Description "Validates script properties" {
-    $ConfigurationIsValid = $False
-    :AllowedConfigurationLoop foreach ($AllowedConfiguration in $AllowedConfigurations) {
-        if ($Configuration -eq $AllowedConfiguration)
-        {
-            $ConfigurationIsValid = $True
-            break AllowedConfigurationLoop
-        }
-    }
-    if ($ConfigurationIsValid -eq $False)
+Task ValidateProperties -Depends InstantiateBuildHelper -Description "Validates script properties" {
+    if ($Global:BuildHelper.IsConfigurationAllowed($Configuration) -eq $False)
     {
-        Throw "'$Configuration' is not a valid configuration. Valid configurations are $AllowedConfigurations."
+        Throw "'$Configuration' is not an allowed configuration. Allowed configurations are $Global:AllowedConfigurations."
     }
-    $PlatformIsValid = $False
-    :AllowedPlatformLoop foreach ($AllowedPlatform in $AllowedPlatforms) {
-        if ($Platform -eq $AllowedPlatform)
+    if ($Global:BuildHelper.IsPlatformAllowed($Platform) -eq $False)
+    {
+        Throw "'$Platform' is not an allowed platform. Allowed platforms are $Global:AllowedPlatforms."
+    }
+}
+
+
+Task Build -Depends ValidateProperties, Clean, InstantiateBuildHelper -Description "Builds all code" {
+    $SolutionFiles = $Global:BuildHelper.GetSolutionFiles()
+    :SolutionFileLoop foreach ($SolutionFile in $SolutionFiles) {
+        Write-Output ("  Building solution '" + $SolutionFile.FullName + "'")
+        Exec { MsBuild $SolutionFile.FullName /nologo /verbosity:minimal /maxcpucount /p:Configuration=$Configuration /p:Platform=$Platform }
+    }
+}
+
+
+Task Test -Depends Build, InstantiateBuildHelper -Description "Runs all tests" {
+    $XunitConsoleExe = [System.IO.Path]::Combine($Global:BuildHelper.GetToolDirectory("xunit").FullName, "xunit.console.exe")
+    $TestFiles = $Global:BuildHelper.GetTestFiles()
+    :TestFileLoop foreach ($TestFile in $TestFiles) {
+        Exec { & $XunitConsoleExe $TestFile.FullName }
+    }
+}
+
+
+Task InstantiateBuildHelper -Description "Creates a global Build.Helper object" {
+
+    $BuildHelperType = "
+    namespace Build
+    {
+        using System;
+        using System.Collections.Generic;
+        using System.IO;
+
+        public class Helper
         {
-            $PlatformIsValid = $True
-            break AllowedPlatformLoop
+            public Helper(string[] allowedConfigurations, string[] allowedPlatforms)
+            {
+                this.AllowedConfigurations = new List<string>(allowedConfigurations);
+                this.AllowedPlatforms = new List<string>(allowedPlatforms);
+                this.SetDirectories();
+                this.DiscoverTooling();
+            }
+
+            private IList<string> AllowedConfigurations { get; set; }
+
+            private IList<string> AllowedPlatforms { get; set; }
+
+            private DirectoryInfo RootDirectory { get; set; }
+
+            private DirectoryInfo ObjectDirectory { get; set; }
+
+            private DirectoryInfo BinariesDirectory { get; set; }
+
+            private DirectoryInfo SolutionDirectory { get; set; }
+
+            private DirectoryInfo ToolsDirectory { get; set; }
+
+            private IDictionary<string, DirectoryInfo> Tools { get; set; }
+
+            public void Clean()
+            {
+                this.RemoveDirectory(this.ObjectDirectory);
+                this.RemoveDirectory(this.BinariesDirectory);
+            }
+
+            public IEnumerable<FileInfo> GetTestFiles()
+            {
+                if (!this.BinariesDirectory.Exists)
+                {
+                    throw new Exception(string.Format(""Binaries directory '{0}' does not exist!"", this.BinariesDirectory.FullName));
+                }
+
+                return this.BinariesDirectory.GetFiles(""*.Facts.dll"", SearchOption.AllDirectories);
+            }
+
+            public IEnumerable<FileInfo> GetSolutionFiles()
+            {
+                if (!this.SolutionDirectory.Exists)
+                {
+                    throw new Exception(string.Format(""Solution directory '{0}' does not exist!"", this.SolutionDirectory.FullName));
+                }
+
+                return this.SolutionDirectory.GetFiles(""*.sln"", SearchOption.TopDirectoryOnly);
+            }
+
+            public DirectoryInfo GetToolDirectory(string toolName)
+            {
+                if (!this.Tools.ContainsKey(toolName))
+                {
+                    throw new Exception(string.Format(""Tool '{0}' cannot be found in '{1}'"", toolName, this.ToolsDirectory.FullName));
+                }
+
+                return this.Tools[toolName];
+            }
+
+            public bool IsConfigurationAllowed(string configuration)
+            {
+                return this.AllowedConfigurations.Contains(configuration);
+            }
+
+            public bool IsPlatformAllowed(string platform)
+            {
+                return this.AllowedPlatforms.Contains(platform);
+            }
+
+            private void DiscoverTooling()
+            {
+                this.Tools = new Dictionary<string, DirectoryInfo>();
+
+                foreach(var toolDirectory in this.ToolsDirectory.GetDirectories(""*"", SearchOption.TopDirectoryOnly))
+                {
+                    DirectoryInfo version = null;
+
+                    foreach(var versionDirectory in toolDirectory.GetDirectories(""*"", SearchOption.TopDirectoryOnly))
+                    {
+                        if (version == null || string.Compare(versionDirectory.Name, version.Name, StringComparison.Ordinal) > 0)
+                        {
+                            version = versionDirectory;
+                        }
+                    }
+
+                    if (version != null)
+                    {
+                        this.Tools.Add(toolDirectory.Name, version);
+                    }
+                }
+            }
+
+            private void RemoveDirectory(DirectoryInfo directory)
+            {
+                if (directory.Exists)
+                {
+                    Console.WriteLine(""  Removing directory '{0}'"", directory.FullName);
+                    directory.Delete(true);
+                }
+            }
+
+            private void SetDirectories()
+            {
+                this.RootDirectory = new DirectoryInfo("".."");
+                this.ObjectDirectory = new DirectoryInfo(Path.Combine(this.RootDirectory.FullName, ""obj""));
+                this.BinariesDirectory = new DirectoryInfo(Path.Combine(this.RootDirectory.FullName, ""bin""));
+                this.SolutionDirectory = new DirectoryInfo(Path.Combine(this.RootDirectory.FullName, ""sln""));
+                this.ToolsDirectory = new DirectoryInfo(Path.Combine(this.RootDirectory.FullName, ""tools""));
+            }
         }
     }
-    if ($PlatformIsValid -eq $False)
-    {
-        Throw "'$Platform' is not a valid platform. Valid platforms are $AllowedPlatforms."
-    }
-}
+    "
+    Add-Type -TypeDefinition $BuildHelperType -Language CSharpVersion3
 
-
-Task Build -Depends ValidateProperties, Clean -Description "Builds all code" {
-    :SolutionLoop foreach ($Solution in $Solutions) {
-        Write-Output ("  Building solution '" + $Solution.FullName + "'")
-        Exec { MsBuild $Solution.FullName /nologo /verbosity:minimal /maxcpucount /p:Configuration=$Configuration /p:Platform=$Platform }
-    }
-}
-
-Task Test -Depends Build -Description "Runs all tests" {
-    $XunitConsoleExe = $RootDirectory.FullName + "\tools\xunit\$XunitVersion\xunit.console.exe"
-
-    $FactBinaries = $BinariesDirectory.GetFiles("*.Facts.dll", [System.IO.SearchOption]::AllDirectories)
-
-    :FactBinaryLoop foreach ($FactBinary in $FactBinaries) {
-        Exec { & $XunitConsoleExe $FactBinary.FullName }
-    }
+    $Global:BuildHelper = New-Object -TypeName Build.Helper -ArgumentList $Global:AllowedConfigurations, $Global:AllowedPlatforms
 }
