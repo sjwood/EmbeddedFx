d5cd859e085a53a2fbf4d8612a7ef2998e4775f8
sjwood <octocat@nym.hush.com>
2013-09-20 14:45:31 +0100

    Added another sample

---
+++ src/Program.WithEmbeddedComplexLib.UsingSimpleLib/Program.cs
@@ -0,0 +1,46 @@
+﻿// Copyright 2012-2013 Steve Wood
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1634:FileHeaderMustShowCopyright", Justification = "Documentation ignored at present time.")]
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1639:FileHeaderMustHaveSummary", Justification = "Documentation ignored at present time.")]
+
+namespace Program.WithEmbeddedComplexLib.UsingSimpleLib
+{
+    using System;
+    using EmbeddedFx;
+    using SimpleLib;
+
+    public class Program
+    {
+        static Program()
+        {
+            EmbeddedAssemblyLoader.Register();
+        }
+
+        public static int Main(string[] args)
+        {
+            try
+            {
+                new Class1();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
