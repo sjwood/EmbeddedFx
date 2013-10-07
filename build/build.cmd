44546d6c26a0f3bd3b6838d0cd046cbbd5426ccf
sjwood <octocat@nym.hush.com>
2013-10-08 00:47:42 +0100

    Added quotes around paths in embedded powershell commands

--- build/build.cmd
+++ build/build.cmd
@@ -46,8 +46,8 @@ if ($CurrentExecutionPolicy -gt [Microsoft.PowerShell.ExecutionPolicy]::RemoteSi
 { ^
   Set-ExecutionPolicy -Scope CurrentUser RemoteSigned; ^
 } ^
-Import-Module "%~dp0Psake\%PSAKE_VERSION%\psake.psm1"; ^
-Invoke-psake -buildFile "%~dp0build.ps1" %*; ^
+Import-Module """%~dp0Psake\%PSAKE_VERSION%\psake.psm1"""; ^
+Invoke-psake -buildFile """%~dp0build.ps1""" %*; ^
 $ReturnCode = 1; ^
 if ($Psake.Build_Success -eq $True) ^
 { ^
